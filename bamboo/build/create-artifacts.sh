57e2c247c88aa50f5d46e7b1ae882a6124a44067
Michael Brandt <michaelbrandt5@gmail.com>
2020-09-10 15:59:18 -0600

    build with ESROOT and KIBANAROOT URLs

--- bamboo/build/create-artifacts.sh
+++ bamboo/build/create-artifacts.sh
@@ -4,10 +4,11 @@ set -e
 # Required environment variables in bamboo:
 #
 # DAAC_NAME=${bamboo.DAAC_NAME}
+# ESROOT_SIT=${bamboo.ESROOT_SIT}
 # HIDE_PDR=${bamboo.HIDE_PDR}
+# KIBANAROOT_SIT=${bamboo.KIBANAROOT_SIT}
 # LABELS=${bamboo.LABELS}
 # SERVED_BY_CUMULUS_API=${bamboo.SERVED_BY_CUMULUS_API}
-
 
 mkdir -p artifacts
 
@@ -19,10 +20,16 @@ for stage in development SIT UAT PROD; do
     APIROOT_VAR=APIROOT_${stage}
     APIROOT=${!APIROOT_VAR}
 
+    KIBANAROOT_VAR=KIBANAROOT_${stage}
+    KIBANAROOT=${!KIBANAROOT_VAR}
+
+    ESROOT_VAR=ESROOT_${stage}
+    ESROOT=${!ESROOT_VAR}
+
     docker run \
         --rm \
         --volume $(pwd)/artifacts:/artifacts \
         cumulus-dashboard:nsidc \
-        bash -c "set -x; APIROOT=${APIROOT} STAGE=${stage} SERVED_BY_CUMULUS_API=${SERVED_BY_CUMULUS_API} DAAC_NAME=${DAAC_NAME} HIDE_PDR=${HIDE_PDR} LABELS=${LABELS} npm run build;\
+        bash -c "set -x; KIBANAROOT=${KIBANAROOT} ESROOT=${ESROOT} APIROOT=${APIROOT} STAGE=${stage} SERVED_BY_CUMULUS_API=${SERVED_BY_CUMULUS_API} DAAC_NAME=${DAAC_NAME} HIDE_PDR=${HIDE_PDR} LABELS=${LABELS} npm run build;\
                  tar -cvzf /artifacts/cumulus-dashboard-dist-${stage}.tar.gz dist"
 done
